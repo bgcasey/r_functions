c05ad974e7ed48da3bb75ed5ffe76f69e70986f7
bgcasey <bgcasey@ualberta.ca>
2024-08-06 07:48:37 -0600

    edited it for the terra function

--- calculate_focal_statistics.R
+++ calculate_focal_statistics.R
@@ -27,69 +27,65 @@ calculate_focal_stat <-
   #' # Apply the function with mean
   #' result_mean <- calculate_focal_stat(r, 500, mean, n_cores = 4)
   #' print(names(result_mean))
-  function(raster_input, window_size_meters,
-           fun, n_cores = 1) {
+  function(raster_input, window_size_meters, fun, n_cores = 1) {
     # Step 1: Get the cell size (assuming x and y resolution are the same)
     cell_size <- res(raster_input)[1]
-
+    
     # Step 2: Calculate window size
     window_size <- ceiling(window_size_meters / cell_size)
-
+    
     # Step 3: Store the original raster band names
     original_names <- names(raster_input)
-
+    
     # Function to process a chunk of the raster
     process_chunk <- function(chunk) {
       focal(chunk,
-        w = matrix(1,
-          nrow = window_size,
-          ncol = window_size
-        ), fun = fun,
-        na.rm = TRUE
+            w = matrix(1,
+                       nrow = window_size,
+                       ncol = window_size
+            ), fun = fun,
+            na.rm = TRUE
       )
     }
-
+    
     # Split the raster into chunks
     split_raster <- function(raster, n_chunks) {
       chunks <- list()
       n_rows <- nrow(raster)
       chunk_size <- ceiling(n_rows / n_chunks)
-
+      
       for (i in 1:n_chunks) {
         start_row <- (i - 1) * chunk_size + 1
         end_row <- min(i * chunk_size, n_rows)
-        chunks[[i]] <- crop(raster, extent(
-          raster, start_row,
-          end_row, 1, ncol(raster)
-        ))
+        chunks[[i]] <- crop(raster, rowFromY(raster, start_row), rowFromY(raster, end_row))
       }
-
+      
       return(chunks)
     }
-
+    
     # Number of chunks
     n_chunks <- n_cores
-
+    
     # Split the raster into chunks
     raster_chunks <- split_raster(raster_input, n_chunks)
-
+    
     # Run the focal stat calculation in parallel
     results <- mclapply(raster_chunks, process_chunk,
-      mc.cores = n_cores
+                        mc.cores = n_cores
     )
-
+    
     # Merge the results back together
     focal_result <- do.call(merge, results)
-
+    
     # Step 5: Determine the function name for band naming
     fun_name <- ifelse(fun == "modal", "mode", fun)
-
+    
     # Step 6: Create new names for the resulting raster bands
     new_names <- paste0(
       original_names, "_", fun_name, "_",
       window_size_meters
     )
     names(focal_result) <- new_names
-
+    
     return(focal_result)
   }
